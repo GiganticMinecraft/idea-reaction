31f0a781de6c4d700da0eccfb5de2d72228ed9a4
m1sk9 <me@m1sk9.dev>
2023-10-12 00:43:02 +0900

    feat: enable intents

--- src/client/discord.rs
+++ src/client/discord.rs
@@ -4,7 +4,8 @@ use serenity::prelude::GatewayIntents;
 use serenity::Client;
 
 pub async fn create_discord_client(token: &str) -> anyhow::Result<Client> {
-    let intents = GatewayIntents::GUILD_MESSAGE_REACTIONS | GatewayIntents::GUILD_MESSAGES;
+    let intents =
+        GatewayIntents::GUILD_MESSAGES | GatewayIntents::GUILDS | GatewayIntents::MESSAGE_CONTENT;
     let client = Client::builder(token, intents)
         .event_handler(EvHandler)
         .await
