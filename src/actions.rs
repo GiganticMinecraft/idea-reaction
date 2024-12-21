4f6ceb90eaa7528bed456490460a0ac23ec66df7
Sho Sakuma <me@m1sk9.dev>
2024-12-21 23:28:50 +0900

    refactor: Enhanced parser error-handling (#152)
    * ci: Add `cargo test` job
    
    * deps: Add `thiserror`
    
    * refactor: Add parser error-handing

--- src/actions.rs
+++ src/actions.rs
@@ -1,5 +1,4 @@
 use crate::redmine::actions::RedmineAction;
-use anyhow::Context as _;
 use serenity::all::{AutoArchiveDuration, ChannelType, ReactionType};
 use serenity::builder::CreateThread;
 use serenity::client::Context;
@@ -16,20 +15,33 @@ pub struct IdeaReactionAction {
     pub issue_number: u16,
 }
 
+#[derive(thiserror::Error, Debug)]
+pub enum IdeaReactionActionError {
+    #[error("Failed to reaction. (Reason: {0})")]
+    FailedToReaction(String),
+    #[error("Failed to create thread. (Reason: {0})")]
+    FailedToCreateThread(String),
+    #[error("Failed to send request to Redmine. (Reason: {0})")]
+    FailedToSendRedmineComment(String),
+}
+
 impl IdeaReactionAction {
-    pub async fn run(&self) -> anyhow::Result<()> {
+    pub async fn run(&self) -> anyhow::Result<(), IdeaReactionActionError> {
         let envs = crate::envs();
 
         // リアクションを付与
         for r in REACTION_EMOJIS {
-            self.message
+            if let Err(why) = self
+                .message
                 .react(&self.ctx.http, ReactionType::Unicode(r.to_string()))
                 .await
-                .context("Failed to reaction.")?;
+            {
+                return Err(IdeaReactionActionError::FailedToReaction(why.to_string()));
+            }
         }
 
         // スレッドを作成
-        let t = self
+        let t = match self
             .message
             .channel_id
             .create_thread_from_message(&self.ctx.http, &self.message.id, {
@@ -42,14 +54,25 @@ impl IdeaReactionAction {
                 .auto_archive_duration(AutoArchiveDuration::OneWeek)
             })
             .await
-            .context("Failed to create thread.")?;
+        {
+            Ok(t) => t,
+            Err(why) => {
+                return Err(IdeaReactionActionError::FailedToCreateThread(
+                    why.to_string(),
+                ));
+            }
+        };
 
         // スレッドのURLを Redmine にコメントする. (Serenity はスレッドの URL を取得するメソッドがない)
         let content = format!(
             "Thread: https://discord.com/channels/{}/{}",
             envs.target_guild_id, t.id
         );
-        RedmineAction::run(self.issue_number, content).await?;
+        if let Err(why) = RedmineAction::run(self.issue_number, content).await {
+            return Err(IdeaReactionActionError::FailedToSendRedmineComment(
+                why.to_string(),
+            ));
+        }
 
         Ok(())
     }
